ed0b2505008b78fbf9dd28d38ca7f05da087ea52
nsette <nicholas.sette@mlb.com>
2024-02-10 11:53:30 -0500

    styling

--- client/my-app/src/components/Closer/CloserPlayersLanding.js
+++ client/my-app/src/components/Closer/CloserPlayersLanding.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 import { useSelector } from 'react-redux';
 import "./styles/CloserPlayers.css"
 import FlipNumbers from 'react-flip-numbers';
-
+import Tooltip from '@mui/material/Tooltip';
+import ReactRoundedImage from "react-rounded-image"
 
 
 
@@ -24,42 +25,26 @@ function CloserPlayersLanding() {
     const [isLoading, setIsLoading] = useState(true);
 
 
-
-
-
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await axios.get(`http://localhost:5555/users/${user.user_id}/players`);
-                setuserPlayers(response.data);
-                setCountedHitters(response.data.all_players.length)
+                const [playersResponse, homerunsResponse] = await Promise.all([
+                    axios.get(`http://localhost:5555/users/${user.user_id}/players?top=5`),
+                    axios.get(`http://localhost:5555/users/${user.user_id}/homeruns?top=5`)
+                ]);
+                setuserPlayers(playersResponse.data);
+                setuserHomeruns(homerunsResponse.data);
+                setCountedHitters(playersResponse.data.all_players.length)
             } catch (error) {
-                console.error('Error fetching user Players:', error);
+                console.error('Error fetching data:', error);
             } finally {
-                setIsLoading(false)
+                setIsLoading(false);
             }
         };
 
         fetchData();
-
     }, []);
 
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(`http://localhost:5555/users/${user.user_id}/homeruns`);
-                setuserHomeruns(response.data);
-            } catch (error) {
-                console.error('Error fetching user homeruns:', error);
-            } finally {
-                setIsLoading(false)
-            }
-        };
-
-        fetchData();
-
-    }, []);
 
 
     useEffect(() => {
@@ -71,6 +56,59 @@ function CloserPlayersLanding() {
             return () => clearTimeout(timeout);
         }
     }, [delayPassed]);
+
+
+    function topData(list) {
+
+        let data
+        switch (list) {
+            case 'players':
+                data = userPlayers.all_players
+                break;
+            case 'pitchers':
+                data = userPlayers.starting_pitchers
+                break;
+            case 'homeruns':
+                data = userHomeruns.home_hitters
+                break;
+
+        }
+
+
+
+        return data?.map((hitter) => {
+            const headshot = `https://img.mlbstatic.com/mlb/images/players/head_shot/${hitter.id}.jpg`;
+            const time_seen = hitter.times_seen;
+            const name = hitter.name;
+
+            return (
+                <Tooltip title={
+                    <div style={{ textAlign: "center" }}>
+                        {name}
+                        < br />
+                        👀: {time_seen}
+                    </div >}
+                    placement="bottom" >
+                    <div className="playerimages" key={hitter.id}>
+                        <a href={`https://www.mlb.com/player/${hitter.id}`} target="_blank">
+                            <ReactRoundedImage
+                                image={headshot}
+                                roundedColor="#66A5CC"
+                                imageWidth="120"
+                                imageHeight="165"
+                                roundedSize="8"
+                                borderRadius="15"
+                                hoverColor="#DD1144"
+                            />
+                        </a>
+                    </div>
+                </Tooltip >
+
+
+            );
+        });
+    }
+
 
 
 
@@ -104,7 +142,6 @@ function CloserPlayersLanding() {
                         width={90}
                         color="white"
                         background="#136d15"
-
                         numberStyle={{ fontSize: '36px' }}
                         numbers={countedHitters.toString()}
                         play
@@ -115,31 +152,36 @@ function CloserPlayersLanding() {
                 <br></br>
                 <p className="players-exact"> To be exact </p>
                 <br></br>
-            </div>
 
-            <div className="closerplayerinfo">
-                <div className="closerhittersseen">
-                    <div>
-                        {
-                            // userHomeruns.home_hitters.map((test) => {
-                            //     console.log(test)
-                            // })
-                        }
+
+
+                <div className='closerData' >
+
+                    <div className='closerDataContainer'>
+                        <p>All Players</p>
+                        <div className="playerimagecontainer">
+                            {topData('players')}
+                        </div>
                     </div>
-                    <p>Hitters</p>
-                </div>
-                <div className="closerpitchersseen">
-                    <p>Pitchers</p>
-                </div>
-                <div className="closerhomerunsseen">
-                    <p>Homeruns</p>
+                    <div className='closerDataContainer'>
+
+                        <p>Starting Pitchers</p>
+                        <div className="playerimagecontainer">
+                            {topData('pitchers')}
+                        </div>
+                    </div>
+
+                    <div className='closerDataContainer'>
+                        <p>Homeruns</p>
+                        <div className="playerimagecontainer">
+                            {topData('homeruns')}
+                        </div>
+                    </div>
+
+
 
                 </div>
-
-
             </div>
-
-
 
 
         </div >
