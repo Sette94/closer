29ffe2351ee841d165c123f4b98cf0e3b402b39a
nsette <nicholas.sette@mlb.com>
2024-02-10 14:16:42 -0500

    Closer done

--- client/my-app/src/components/Closer/CloserEnding.js
+++ client/my-app/src/components/Closer/CloserEnding.js
@@ -4,30 +4,31 @@ import axios from 'axios';
 import { useSelector } from 'react-redux';
 import './styles/CloserLanding.css';
 
+
 function CloserEnding() {
     const navigate = useNavigate();
-
     const user = useSelector((state) => state.user);
-
 
 
     return (
         <div className="closer-container">
-            <h1>Closer: Fun Facts</h1>
+            <div onClick={() => navigate(`/closer/${user.user_id}/general/players`)} className="arrow" style={{ transform: 'rotate(90deg)', top: '15%', left: '5%' }}>
+                <span ></span>
+                <span ></span>
+                <span ></span>
+            </div>
+            <h1>Closer: The 9th Inning</h1>
             <br></br>
-            <p className="greeting-open">Short Porches, Shea Bridges, Walls of Ivy</p>
+            <p className="greeting-open">That closes out your year!</p>
             <br></br>
-            <p className="greeting-seen">Peanuts, Popcorn, Ice Cream</p>
+            <p className="greeting-seen">While the game ends now it will begin again the spring</p>
             <br></br>
-            <p className="greeting-exact">Summer nights that won't end</p>
+            <p className="greeting-exact">When everything else begins again</p>
             <br></br>
-            <p className="greeting-fact"> Here are some fun facts on how you spent <br></br>your time at the Ballpark</p>
-            <br></br>
-            <button className="greeting-close" onClick={() => { navigate(`/closer/${user.user_id}/general/facts`) }}>Fun Facts</button>
-
-
+            <div className='greeting-fact' id='closergoodbye'></div>
         </div>
-    );
+    )
 }
+
 export default CloserEnding
 
--- client/my-app/src/routes.js
+++ client/my-app/src/routes.js
@@ -9,7 +9,6 @@ import CloserGameComponent from "./components/Closer/CloserGameComponent";
 import CloserFunFactsLanding from "./components/Closer/CloserFunFactsLanding"
 import CloserFunFacts from "./components/Closer/CloserFunFacts"
 import CloserPlayersLanding from "./components/Closer/CloserPlayersLanding"
-import CloserPlayersFacts from "./components/Closer/CloserPlayersFacts"
 import CloserEnding from "./components/Closer/CloserEnding"
 
 
@@ -32,7 +31,6 @@ const routes = [
             { path: "/closer/:userId/general", element: <CloserFunFactsLanding /> },
             { path: "/closer/:userId/general/facts", element: <CloserFunFacts /> },
             { path: "/closer/:userId/general/players", element: <CloserPlayersLanding /> },
-            { path: "/closer/:userId/general/players/facts", element: <CloserPlayersFacts /> },
             { path: "/closer/:userId/ending", element: <CloserEnding /> }
 
 
