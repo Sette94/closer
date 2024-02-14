c05e3b02ebf8b0f4783f81f86cd3cbd7f85a5517
nsette <nicholas.sette@mlb.com>
2024-02-14 12:56:14 -0500

    updating DB routing

--- server/app.py
+++ server/app.py
@@ -20,9 +20,9 @@ app = Flask(__name__)
 CORS(app)
 
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 migrate = Migrate(app, db)
